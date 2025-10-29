f1bc53ab6d1bf0ae0e79ac7dba8e91cc019d394b
Rana Salem <ranatasalem@gmail.com>
2025-10-29 17:03:31 +0000

    general tidy up

--- src/scraper.py
+++ src/scraper.py
@@ -5,20 +5,21 @@ from bs4 import BeautifulSoup
 
 load_dotenv()
 
-gcc_url = os.getenv("GCC_URL")
+uprn = os.getenv('UPRN')
+gcc_url = f'https://onlineservices.glasgow.gov.uk/forms/refuseandrecyclingcalendar/CollectionsCalendar.aspx?UPRN={uprn}'
 page = requests.get(gcc_url)
-soup = BeautifulSoup(page.text, "html.parser")
+soup = BeautifulSoup(page.text, 'html.parser')
 
-all_collection_dates = soup.find_all("td", class_="CalendarDayStyle")
+all_collection_dates = soup.find_all('td', class_='CalendarDayStyle')
 
 data = []
 
 for collection_date in all_collection_dates:
   item = {}
 
-  item["Date"] = collection_date.get("title")
-  imgs = collection_date.find_all("img")
-  item["Bin colour"] = [img.get("title").partition(' ')[0] for img in imgs] if imgs else "No bins due for collection"
+  item['Date'] = collection_date.get('title')
+  imgs = collection_date.find_all('img')
+  item['Bin colour'] = [img.get('title').partition(' ')[0] for img in imgs] if imgs else 'No bins due for collection'
 
   data.append(item)
 
