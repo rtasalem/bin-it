7030a58fb6abc7b2386bfbd87deeeb32a5bf2396
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2025-10-31 10:29:47 +0000

    003 Set up database (#3)

--- src/db/client.py
+++ src/db/client.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+load_dotenv()
+
 uri = os.getenv('MONGO_URI')
 client = MongoClient(uri)
-database = client['bin_it']
-collection = database.create_collection('bin_collection_dates')
+
+__all__ = ['client']
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from save_scraped_data import save_scraped_data
+
+def main():
+  print('🗑️ Starting scraper...')
+
+  save_scraped_data()
+
+if __name__ == '__main__':
+  main()
