b8c11be5107ee7f449396dfa0136da1064af1872
Rana Salem <ranatasalem@gmail.com>
2025-11-13 23:36:07 +0000

    updates

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from mongo.indexes.bin_collections import set_up_bin_collections_index
 from data.save_scraped_data import save_scraped_data
-from send_email_reminder import send_email_reminder
-
 import os
 from dotenv import load_dotenv
 
@@ -12,8 +10,6 @@ def main():
 
   set_up_bin_collections_index()
   save_scraped_data()
-
-  send_email_reminder('test', 'hello world', sender, recipient)
 
 if __name__ == '__main__':
   main()
