dfe4ea86363584f81865baeda25496fdbd04a08c
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2025-11-20 12:02:22 +0000

    008 Configure cron jobs (#8)

---
+++ src/jobs/weekly_reminders.py
@@ -0,0 +1,13 @@
+from jobs.scheduler import scheduler
+from alerts.check_and_alert import check_and_alert
+
+def send_weekly_bin_collection_reminder():
+  scheduler.add_job(
+    check_and_alert,
+    trigger='cron',
+    day_of_week='mon-fri',
+    hour=12,
+    minute=0
+  )
+
+__all__ = ['send_weekly_bin_collection_reminder']
--- src/main.py
+++ src/main.py
@@ -3,14 +3,16 @@ import os
 from dotenv import load_dotenv
 from mongo.indexes.bin_collections import set_up_bin_collections_index
 from data.save_scraped_data import save_scraped_data
+from jobs.monthly_database_refresh import execute_monthly_database_refresh
+from jobs.weekly_reminders import send_weekly_bin_collection_reminder
 
 def main():
   load_dotenv()
 
-  print('🗑️ Starting scraper...')
-
   set_up_bin_collections_index()
   save_scraped_data()
+  execute_monthly_database_refresh()
+  send_weekly_bin_collection_reminder()
 
   while True:
     time.sleep(1)
