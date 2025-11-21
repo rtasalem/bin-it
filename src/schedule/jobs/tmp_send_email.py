28f357e400feeb8a7f0f5a1ed96d7cd5174e66fd
Rana Salem <ranatasalem@gmail.com>
2025-11-21 10:18:24 +0000

    delete tmp email send test

--- src/schedule/jobs/tmp_send_email.py
+++
@@ -1,9 +0,0 @@
-from schedule.scheduler import scheduler
-from alerts.tmp_email import tmp_send_email_alert
-
-def tmp_send_outlook_email():
-  scheduler.add_job(
-    tmp_send_email_alert,
-    trigger='cron',
-    minute=17
-  )
