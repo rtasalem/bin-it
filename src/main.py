58808cd2443b4bdcfc44fa3bcef2b1aafb26a008
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2025-11-25 22:47:46 +0000

    011 Configure CI/CD infrastructure (#11)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-import time
 import os
 from dotenv import load_dotenv
 from mongo.indexes.bin_collections import setup_bin_collections_index
-from handle_scraped_data import handle_scraped_data
+from data.handle_scraped_data import handle_scraped_data
 from schedule.jobs.monthly_database_refresh import execute_monthly_database_refresh
 from schedule.jobs.weekly_reminders import send_weekly_bin_collection_reminder
+from schedule.scheduler import scheduler
+from alerts.handle_alerts import handle_bin_collection_alerts
 
 def main():
   load_dotenv()
@@ -14,8 +15,7 @@ def main():
   execute_monthly_database_refresh()
   send_weekly_bin_collection_reminder()
 
-  while True:
-    time.sleep(1)
+  scheduler.start()
 
 if __name__ == '__main__':
   main()
