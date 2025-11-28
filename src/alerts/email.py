04b897c13cfd88cf75a7b01b1c60e304732f8323
Rana Salem <ranatasalem@gmail.com>
2025-11-28 01:11:46 +0000

    update email template

--- src/alerts/email.py
+++ src/alerts/email.py
@@ -22,7 +22,7 @@ def send_email_alert(collection_date, bin_colours):
   recipient = os.getenv('RECIPIENT')
 
   msg = MIMEText(body)
-  msg['Subject'] = '⚠️ Bin collection due tomorrow!'
+  msg['Subject'] = '⚠️ Bin collection due tomorrow'
   msg['From'] = sender
   msg['To'] = recipient
 
